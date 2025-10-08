51dffdbeb9c9e80375b043383786da385c259098
archie-codes <bauzonarchie@gmail.com>
2025-10-08 16:01:50 +0800

    Update Space Link

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,7 +4,7 @@ import OverviewCounter from "../overview-counter/OverviewCounter";
 import BannerDetails from "../bannerDetails/BannerDetails";
 import ValueCard from '../valueCard/ValueCard';
 import News from "../news/News";
-import Space from '../space/space';
+import Space from '../space/Space';
 import NewsSlider from "../stations/NewsSlider";
 import Footer from "../footer/Footer";
 import "aos/dist/aos.css";
